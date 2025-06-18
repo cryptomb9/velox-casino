e40a2e28ff0a489010fb26312274adf5d1fa9478
cryptomb9 <youremail@example.com>
2025-06-18 22:13:47 +0100

    edit description

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ import Layout from "../components/layout";
 export default function Home() {
   return (
     <Layout>
-      <h1 className="text-4xl font-bold mb-4">Welcome to Velox Casino 🎰</h1>
-      <p className="text-lg mb-6">A Web3-powered gaming hub built on Monad. Win points, climb the leaderboard, and stay ready for future rewards.</p>
+      <h1 className="text-4xl font-bold mb-4">Welcome to Velox 🎰</h1>
+      <p className="text-lg mb-6">A Web3-powered gaming hub built on Monad.Flip coin, click fast,and gamble your way to glory</p>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-10">
         <a href="/click" className="bg-white text-black p-6 rounded-2xl shadow hover:scale-105 transition">
@@ -21,7 +21,7 @@ export default function Home() {
       <section className="bg-black bg-opacity-40 p-6 rounded-xl text-white">
         <h3 className="text-2xl font-semibold mb-2">🔥 How It Works</h3>
         <ul className="list-disc pl-5 space-y-2">
-          <li>Play games daily to double your MON or MB tokens (VP).</li>
+          <li>Play games daily to double your MON or MB tokens .</li>
           <li>Check your wallet and profile from the top-right menu.</li>
         </ul>
       </section>
